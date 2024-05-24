1a04686bf4a910def9429c211decee2443b22e30
Victor Hugo <56604707+Vituinha@users.noreply.github.com>
2024-05-24 13:33:35 -0300

    Adicionando Arquivos
    Adicionando novos arquivos

---
+++ CNO/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CNO
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ItemList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ItemList.SelectedItem is ListBoxItem selectedItem)
+            {
+                string itemContent = selectedItem.Content.ToString();
+                AddTextBlockToCanvas(itemContent);
+            }
+        }
+
+        private void AddTextBlockToCanvas(string text)
+        {
+            TextBlock textBlock = new TextBlock
+            {
+                Text = text,
+                Name = text, // Definindo o nome do objeto como o texto
+                Margin = new Thickness(10)
+            };
+
+            ItemCanvas.Children.Add(textBlock);
+        }
+    }
+}
